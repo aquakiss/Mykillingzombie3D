d3711b7f93f9c3eff36c3c83a09fd27f83796325
Chaoskira <fredverio@hotmail.fr>
2014-05-21 01:05:43 +0200

    ajout script follow

--- My-killing-zomby/Assets/Scène1/Scripts/spawn.cs
+++ My-killing-zomby/Assets/Scène1/Scripts/spawn.cs
@@ -10,12 +10,16 @@ public class spawn : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 			while (ennemy > 0) {
-				GameObject en = Instantiate (ennemi, transform.position, transform.rotation) as GameObject;	
-				en.gameObject.renderer.material.color = Color.red;
-				en.GetComponent<NavMeshAgent> ().destination = m_player.position;		
-				ennemy = ennemy - 1;
+					spawnen ();
+					ennemy = ennemy - 1;
 			}
-			
-			
+	}
+
+	public void spawnen(){
+		GameObject en = Instantiate (ennemi, transform.position, transform.rotation) as GameObject;	
+		en.gameObject.renderer.material.color = Color.red;
+		en.GetComponent<NavMeshAgent> ().destination = m_player.position;
+
 	}
 }
+
