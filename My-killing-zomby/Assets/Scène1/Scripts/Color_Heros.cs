ebcd35bb838ca1c6bd866709d783066286e35399
aquakiss <romain.menard17@gmail.com>
2014-05-20 23:03:19 +0200

    avancement système de tir

--- My-killing-zomby/Assets/Scène1/Scripts/Color_Heros.cs
+++ My-killing-zomby/Assets/Scène1/Scripts/Color_Heros.cs
@@ -3,28 +3,81 @@ using System.Collections;
 
 [System.Serializable]
 
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
 public class Color_Heros : MonoBehaviour {
 
+	public float sped;
 	public float maxSpeed = 1f;
+	public float tilt;
+	public Boundary boundary;
+	
+	public GameObject Bullet;
+	public Transform BulletSpawn;
+	public float fireRate;
+	
+	private float nextFire;
 
 	public Vector3 speed = new Vector3(30, 0, 30);
 
 	private Vector3 movement;
 
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		gameObject.renderer.material.color = Color.blue;
 		float inputX = Input.GetAxis("Vertical");
 		float inputZ = Input.GetAxis("Horizontal");
 		movement = new Vector3(speed.x * inputX, 0, speed.z * -inputZ * Time.deltaTime * 100);
-	}
 
-	void FixedUpdate()
-	{
-		
 		rigidbody.velocity = movement;
 	}
-}
+
+
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(Bullet, BulletSpawn.position, BulletSpawn.rotation);
+		}
+	}
+}   
 		
+
+
+
+	/*
+	void Update ()
+	{
+		gameObject.renderer.material.color = Color.blue;
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(Bullet, BulletSpawn.position, BulletSpawn.rotation);
+			//audio.Play ();
+		}
+	}
 	
+	void FixedUpdate ()
+	{
+		gameObject.renderer.material.color = Color.blue;
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputZ = Input.GetAxis ("Vertical");
+		
+		movement = new Vector3 (inputX, 0.0f, inputZ);
+		rigidbody.velocity = movement;
+		
+		rigidbody.position = new Vector3 
+			(
+				Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax), 
+				0.0f, 
+				Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
+				);
+		
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+	}
+}	*/
 
---
+++ My-killing-zomby/Assets/Scène1/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	public float speed;
+
+	void Start ()
+	{
+		rigidbody.velocity = transform.forward * speed;
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.tag == "Ennemis") {
+			print ("collision avec player, je le tue");
+			//rigidbody2D.velocity = movements;
+		} else if (col.gameObject.tag == "Décor") {
+			print ("j'ai toucher un mur, je bouge");
+			//this.gameObject.transform.rotation;
+		}
+	}
+}
