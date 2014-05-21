340f493ed2aebc15981cc583f4af40196b5beee1
aquakiss <romain.menard17@gmail.com>
2014-05-21 02:02:32 +0200

    nos amis les mines

---
+++ My-killing-zomby/Assets/Mine/Mine.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mine : MonoBehaviour {
+
+	public GameObject explosionPrefab;
+	public float force=10f;
+	public string tagKeyWordToActivate = "ennemy";
+
+	public void OnTriggerEnter(Collider col )
+	{
+		if (col.gameObject.tag.Equals (tagKeyWordToActivate)) {
+			Explode();		
+			Kill(col.gameObject);
+		}
+	}
+
+	public void Explode()
+	{
+//		Debug.Log ("Boum");	
+		if(explosionPrefab!=null)
+		{
+			GameObject gamo =GameObject.Instantiate (explosionPrefab, transform.position, transform.rotation) as GameObject;
+			Destroy(gamo,10);
+		}
+		Destroy(this.gameObject);
+	}
+	public void Kill(GameObject gamo)
+	{
+
+		if(gamo==null)return;
+
+		NavMeshAgent agent = gamo.GetComponent<NavMeshAgent> () as NavMeshAgent;
+		if(agent!=null)
+			Destroy (agent);
+		Collider shell = gamo.GetComponent<Collider> () as Collider;
+		if(shell!=null)
+			Destroy (shell);
+
+		if (gamo.rigidbody != null)
+			gamo.rigidbody.AddExplosionForce (force, transform.position, 2);
+		Destroy (gamo, 2);
+	}
+}
---
+++ My-killing-zomby/Assets/Mine/MineDropper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineDropper : MonoBehaviour {
+
+	public KeyCode [] keyboardInputToDrop;
+	public GameObject prefabMine;
+	public float minTimeBetween=2f;
+	public float whenLastMineDrop;
+
+
+	// Update is called once per frame
+	void Update () {
+		if (prefabMine != null) {
+				bool askDropMine = false;
+				foreach (KeyCode kc in keyboardInputToDrop) {
+						if (Input.GetKeyDown (kc))
+								askDropMine = true;	
+				}
+
+				if (askDropMine) {
+						float time = Time.timeSinceLevelLoad;
+						if (time - whenLastMineDrop > minTimeBetween) {
+								whenLastMineDrop = time;
+								DropMine ();
+						}
+				}
+		}
+	}
+	public void DropMine()
+	{
+		GameObject.Instantiate (prefabMine, transform.position, transform.rotation);
+	}
+}
