4499812ac80faf722b57b792f344478288617d05
Chaoskira <fredverio@hotmail.fr>
2014-05-21 09:44:49 +0200

    ajout système de vague + ajout nouveau spawner

--- My-killing-zomby/Assets/Scène1/Scripts/spawn.cs
+++ My-killing-zomby/Assets/Scène1/Scripts/spawn.cs
@@ -6,20 +6,34 @@ public class spawn : MonoBehaviour {
 	public GameObject ennemi;
 	public Transform m_player;
 	public int ennemy;
+	public float lastRefresh;
+	public float spawnDelay = 15f;
+	public int nombreVague = 4;
 
 	// Update is called once per frame
+	void Start(){
+		spawnen ();
+	}
+
+
+
 	void Update () {
-			while (ennemy > 0) {
-					spawnen ();
-					ennemy = ennemy - 1;
-			}
+		float time = Time.timeSinceLevelLoad;
+		if (time - lastRefresh > spawnDelay && nombreVague > 0) {
+			lastRefresh = time;
+			spawnen ();
+		}
 	}
 
 	public void spawnen(){
-		GameObject en = Instantiate (ennemi, transform.position, transform.rotation) as GameObject;	
-		en.gameObject.renderer.material.color = Color.red;
-		en.GetComponent<NavMeshAgent> ().destination = m_player.position;
-
+		while (ennemy > 0) {
+			GameObject en = Instantiate (ennemi, transform.position, transform.rotation) as GameObject;	
+			en.gameObject.renderer.material.color = Color.red;
+			en.GetComponent<NavMeshAgent> ().destination = m_player.position;
+			ennemy = ennemy - 1;
+		}
+		ennemy = 3;
+		nombreVague--;
 	}
 }
 
